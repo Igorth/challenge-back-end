5b8b6a44f15fbf943e9ce3ae6d366f8e696fe843
Igor Dias <igordiasth@gmail.com>
2020-04-08 21:45:14 +1200

    able to delete, validation delete

--- src/app.js
+++ src/app.js
@@ -50,7 +50,17 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
